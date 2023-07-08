818867ac618f804f0d9947851c7a2e1603877d3f
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-07-08 20:54:54 +0700

    add model Employment & Company

--- models/employee.go
+++ models/employee.go
@@ -15,4 +15,15 @@ type Employee struct {
 	DepartmentID uint
 	Department   Department `gorm:"foreignKey:DepartmentID"`
 	Position     string
+	Employment   Employment
+}
+
+type Employment struct {
+	CompanyID  uint
+	Company    Company `gorm:"foreignKey:CompanyID"`
+	EmployeeID uint
+}
+
+type Company struct {
+	Name string
 }
