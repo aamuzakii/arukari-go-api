5881bd1475592d555cd4df2ae03a32d96c374dce
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-07-08 18:39:21 +0700

    add field in model

--- models/employee.go
+++ models/employee.go
@@ -4,12 +4,14 @@ import "gorm.io/gorm"
 
 type Department struct {
 	gorm.Model
-	Name string
+	Name        string
+	Description string
 }
 
 type Employee struct {
 	gorm.Model
 	Name         string
+	Email        string
 	DepartmentID uint
 	Department   Department `gorm:"foreignKey:DepartmentID"`
 	Position     string
